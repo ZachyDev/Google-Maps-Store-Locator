1060d88d94c7cc25ab4ffedcd91cc07230a90c74
ZachyDev <mosetizachary001@gmail.com>
2020-05-27 18:21:31 +0300

    added firebase configuration for hosting

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBLaEfhoJBDxhrxYVVj3Zoj6lDHrcjgK24",
+    authDomain: "store-locator-f216f.firebaseapp.com",
+    databaseURL: "https://store-locator-f216f.firebaseio.com",
+    projectId: "store-locator-f216f",
+    storageBucket: "store-locator-f216f.appspot.com",
+    messagingSenderId: "410157577182",
+    appId: "1:410157577182:web:25b310f28c3c52e283e34f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
